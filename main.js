6ffa37097dc797a891e64b1cef90cc8e4dfd1144
Bingo <faaa@live.com>
2022-01-28 23:22:54 +0800

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello im.bingo !</h1>
+  <h1>你好，欢迎来到 im.bingo。感谢访问。 </h1>
+  <h2>我是刘俊 bingo。网站还在建设中。</h2>
+  <h3>你可以访问我的 <a href="https://github.com/bin9o">GitHub 主页</a> </h3>
+  <h3>你也可以访问 <a href="https://three.bingo">three.bingo</a> 了解我的 <a href="https://threejs.org">Three.js</a> Demo</h3>
+  <h3>📧 faaa#live.com</h3>
+`;

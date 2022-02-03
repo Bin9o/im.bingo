165d4378a2e2fa1eb42584a7a84b24a3798c1100
Bingo <faaa@live.com>
2022-02-03 16:45:21 +0800

    update css

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello im.bingo !</h1>
-  <h1>你好，欢迎来到 im.bingo。感谢访问。 </h1>
-  <h2>我是刘俊 bingo。网站还在建设中。</h2>
-  <h3>你可以访问我的 <a href="https://github.com/bin9o">GitHub 主页</a> </h3>
-  <h3>你也可以访问 <a href="https://three.bingo">three.bingo</a> 了解我的 <a href="https://threejs.org">Three.js</a> Demo</h3>
-  <h3>📧 faaa#live.com</h3>
+  <div class="container">
+    <header>
+      <h1>im.bingo</h1>
+    </header>
+
+    <div class="main clearfix">
+      <div>
+        <div class="media">
+          <div class="media-body">
+            <h4>你好，欢迎来到 im.bingo。感谢访问。 </h4>
+            <p>
+              我是刘俊 bingo。网站还在建设中。
+            </p>
+            <p>
+              你可以访问我的 <a href="https://github.com/bin9o">GitHub 主页</a>
+            </p>
+            <p>
+              你也可以访问 <a href="https://three.bingo">three.bingo</a> 了解我的 <a href="https://threejs.org">Three.js</a> Demo
+            </p>
+            <p>
+              我的邮箱📮：BingJun@outlook.com
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
 `;
